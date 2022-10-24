076e065226060c9e3df3c01f0500006dae5d1ab8
zlesaja1 <zlesaja1@uhk.cz>
2022-10-24 12:09:24 +0200

    Chat app init commit

--- src/models/Message.java
+++ src/models/Message.java
@@ -8,10 +8,25 @@ public class Message {
     private String text;
     private LocalDateTime created;
 
+    public static final int USER_LOGGED_IN = 1;
+    public static final int USER_LOGGED_OUT = 2;
+
+    private static final String AUTHOR_SYSTEM = "System";
+
     public Message(String author, String text) {
         this.author = author;
         this.text = text;
         this.created = LocalDateTime.now();
+    }
+
+    public Message(int type,String userName){
+        this.author = AUTHOR_SYSTEM;
+        created = LocalDateTime.now();
+        if(type == USER_LOGGED_IN){
+            text = userName + " has joined the chat";
+        }else if(type == USER_LOGGED_OUT){
+            text = userName + " has left the chat";
+        }
     }
 
     public String getAuthor() {
